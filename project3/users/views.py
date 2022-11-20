30a2e3283bf3156a1cdb9cd3608f511b0ed37f3e
memorisanka <j.krawczyk87@gmail.com>
2022-11-20 15:05:18 +0100

    feat(registration): add email to registration form

--- project3/users/views.py
+++ project3/users/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegisterForm
+from django.contrib import messages
 
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f"Account has been created. Welcome, {username}!")
+        #     redirect do strony po zalogowaniu
+    else:
+        form = UserRegisterForm()
 
     return render(request, 'users/register.html', {'form': form})
